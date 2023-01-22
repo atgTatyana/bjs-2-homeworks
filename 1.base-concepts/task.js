145a100fdc8cd4be9b3e22be651d29075d298ff8
Tatyana Amosova <atgatg@bk.ru>
2023-01-23 00:56:13 +0300

    Homework_1 Initial

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = b ** 2 - 4 * a * c;
+  if (discriminant === 0) {
+    arr[0] = -b / (2 * a);
+  } else if (discriminant > 0) {
+      arr[0] = (-b + Math.sqrt(discriminant)) / (2 * a);
+      arr[1] = (-b - Math.sqrt(discriminant)) / (2 * a);
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+    return false;
+  }
+  percent = +percent;
+  contribution = +contribution;
+  amount = +amount;
+  countMonths = +countMonths;
+  let percentMonth = percent / 100 / 12;
+  let credit = amount - contribution;
+  let payment = credit * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonths) - 1)));
+  let totalPayment = (contribution + payment * countMonths).toFixed(2);
+  return +totalPayment;
+} 
