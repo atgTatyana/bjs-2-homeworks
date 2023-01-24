f5613af875b51040cf8ff0b367e5701e4b33f38b
Tatyana Amosova <atgatg@bk.ru>
2023-01-24 23:19:32 +0300

    task 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,16 @@
 function getArrayParams(...arr) {
-
+  let min = max = arr[0];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+  let avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
