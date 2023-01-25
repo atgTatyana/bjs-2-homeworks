86852241fe51189db3ef09d4d5f10934670bf881
atgTatyana <atgatg@bk.ru>
2023-01-25 17:19:51 +0300

    task 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -15,21 +15,52 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  return arr.reduce((sum, currentItem) => sum + currentItem);
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  return Math.max(...arr) - Math.min(...arr);
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sumEvenElement = sumOddElement = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0 ) {
+      sumEvenElement += arr[i];
+    } else {
+      sumOddElement += arr[i];
+    }
+  }
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sumEvenElement = countEvenElement = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0 ) {
+      sumEvenElement += arr[i];
+      countEvenElement += 1;
+    } 
+  }
+  return sumEvenElement / countEvenElement;
 }
 
 function makeWork (arrOfArr, func) {
-
+  let maxWorkerResult = func(arrOfArr[0]);
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let result = func(...arrOfArr);
+    
+  }
 }
