0b88ce420b628187257cba9b0763c822e5279ad0
Tatyana Amosova <atgatg@bk.ru>
2023-02-04 13:35:25 +0300

    Homework_4 corrected

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -11,9 +11,10 @@ Student.prototype.setSubject = function (subjectName) {
 
 Student.prototype.addMarks = function (...marksToAdd) {
   if (this.hasOwnProperty('marks')) {
-		for (let mark of marksToAdd) {
-			this.marks.push(mark);
-		}	
+		this.marks.push(...marksToAdd);
+		// for (let mark of marksToAdd) {
+		// 	this.marks.push(mark);
+		// }	
 	}
 }
 
